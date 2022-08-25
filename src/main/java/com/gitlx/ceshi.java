eb8c3b1304a2184e61e348137ed217edb6a401bf
qzf <1197605622@qq.com>
2022-08-25 22:19:22 +0800

    分支添加dev

--- src/main/java/com/gitlx/ceshi.java
+++ src/main/java/com/gitlx/ceshi.java
@@ -8,5 +8,6 @@ public class ceshi {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("更新1");
+        System.out.println("分支添加dev");
     }
 }
